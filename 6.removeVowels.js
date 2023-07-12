ae5ebad3a26d4e9dae4b4d60b6ca0689fd36877b
psbharadwaj2002 <138998317+psbharadwaj2002@users.noreply.github.com>
2023-07-12 19:54:21 +0530

    Update 6.removeVowels.js

--- 6.removeVowels.js
+++ 6.removeVowels.js
@@ -1,4 +1,4 @@
-// https://practice.geeksforgeeks.org/problems/remove-vowels-from-string1446/1?page=3&difficulty%5B%5D=-2&sortBy=submissions
+// // https://practice.geeksforgeeks.org/problems/remove-vowels-from-string1446/1?page=3&difficulty%5B%5D=-2&sortBy=submissions
 
 function removeVowels(word){
     let str = "";
